c1a748ef4957323cae30ab196908042bc0336129
fahim- hossen <fahim.fahim3408@gmail.com>
2024-08-04 04:42:20 +0600

    reunit

---
+++ tutorial/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'fahim',
+    author_email= 'fahim.fahim3408@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
